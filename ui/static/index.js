de131b2156f9e97766427852691401f04126905b
josh <joshuasiems@gmail.com>
2022-02-15 18:04:37 -0600

    Add toggling device list templates

--- ui/static/index.js
+++ ui/static/index.js
@@ -16,6 +16,18 @@ $("body").ready(function() {
         $("#server_response").html(data)
     });
 
+    // onclick functions below
+    $(".device_list_title").click(function(e) {
+        var underscore_location = e.target.id.indexOf("_")
+        var device = e.target.id.substring(0, underscore_location);
+        underscore_location = e.target.id.lastIndexOf("_")
+        var id = e.target.id.substring(underscore_location + 1);
+
+        var css_id = "#" + device + "_" + id;
+        console.log("Toggling open on device list: " + css_id);
+
+        $(css_id).toggleClass("open");
+    });
 
     return;
 });
