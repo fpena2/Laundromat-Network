4b6930f6d586b4175a8f53a7676e170c192b1367
fpena2 <fpena2@unh.newhaven.edu>
2022-03-10 21:41:36 -0600

    Add logging

--- system/run.py
+++ system/run.py
@@ -1,13 +1,18 @@
+import time
 import os
 import sys
+import uuid
 sys.path.append(os.path.abspath("./libs/"))
 from Measurements import Measure
 
 # Initialize Objects 
 a = Measure()
+__device__ = os.environ["USER"]
 
 try: 
-    for i in range(50):
-        print(a.get(2000))
+    filename = str(uuid.uuid4())
+    with open("./logs/{}.csv".format(filename), "w") as f:
+        while True:
+            print("{},{}/n".format(int(time.time()), a.get(2000)), file=f)
 except Exception:
     pass
