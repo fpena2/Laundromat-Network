f4929b529bcfb705594a730e758b10a424aede23
fpena2 <fpena2@unh.newhaven.edu>
2022-03-10 20:07:15 -0600

    Add measurement class

--- system/README.md
+++ system/README.md
@@ -1,1 +1,13 @@
 ## Configuration 
+
+* Enable the I2C interface on the Raspberry Pi using `raspi-config`
+* Install the ADC library:
+    ```
+    $ pip3 install ADS1115==0.2.1
+    ```
+
+### Connect the ADC to the Pi as follows:
+* ADS1x15 VDD to Raspberry Pi 3.3V
+* ADS1x15 GND to Raspberry Pi GND
+* ADS1x15 SCL to Raspberry Pi SCL
+* ADS1x15 SDA to Raspberry Pi SDA
---
+++ system/libs/Measurement.py
@@ -0,0 +1,37 @@
+import time
+from scipy.fftpack import ifft2
+import board
+import busio
+from math import sqrt
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+class Measurement():
+    def __init__(self) -> None:
+        self.FACTOR = 10  # 100A => 10mA => 10V
+        self.ERROR = 1.11
+        # Create the I2C bus
+        i2c = busio.I2C(board.SCL, board.SDA)
+        # Create the ADC object using the I2C bus
+        ads = ADS.ADS1115(i2c)
+        # Create differential input between channel 0 and 1
+        self.chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+    def milli(self):
+        return round(time.time() * 1000)
+
+    def get(self):
+        peak_voltage = 0
+        t = self.milli()
+        while(self.milli() - t < 2000):
+            voltage = self.chan.voltage
+            if voltage > peak_voltage:
+                peak_voltage = voltage
+
+        # Coverts voltage to current
+        return peak_voltage * self.FACTOR * self.ERROR
+
+
+a = Measurement()
+print(a.get())
