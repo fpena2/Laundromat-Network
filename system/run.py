f7dcad8a7f8aeabcee17b8e4a7cad91240f4d2f7
fpena2 <fpena2@unh.newhaven.edu>
2022-03-23 11:16:09 -0500

    Add debug opt

--- system/run.py
+++ system/run.py
@@ -2,12 +2,13 @@ import time
 import os
 import sys
 import uuid
+import argparse
+# Import custom libs 
 sys.path.append(os.path.abspath("./libs/"))
 from Measure import Measure
 from COM import SocketIO
 
 # Initialize Params 
-__device__ = os.environ["USER"]
 url = "ec2-18-191-244-170.us-east-2.compute.amazonaws.com/socketio"
 
 # Initialize Objects 
@@ -16,18 +17,31 @@ cObj = SocketIO(url)
 cObj.setup()
 cObj.run()
 
+# Testing Params
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', dest='__debug__', default=False, action='store_true')
+args = parser.parse_args()
+
 try: 
-    # filename = str(uuid.uuid4())
-    filename = "test"
+    filename = str(uuid.uuid4())
+    if args.__debug__:
+        filename = "test"
+
     with open("./logs/{}.csv".format(filename), "w") as f:
         while True:
             unixTime = str(int(time.time()))
             current = str(mObj.get(interval_ms=2000))
+            msg = "{},{}".format(unixTime, current)
+            
             # Print to file
-            print("{},{}".format(unixTime, current), file=f)
+            print(msg, file=f)
             f.flush()
+
             # Push to server 
             cObj.send(unixTime, current)
+            
+            # Print to console 
+            print(msg)
 
 except Exception as e:
     print(e)
