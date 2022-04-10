f1525d15866dc23c242582fac095a4ea96224eba
fpena2 <fpena2@unh.newhaven.edu>
2022-04-10 16:20:50 -0500

    Fix ploting

--- system/debug/plot_debug.py
+++ system/debug/plot_debug.py
@@ -1,20 +1,30 @@
 import matplotlib.pyplot as plt
 import csv
-  
-unixTime = []
-current = []
+from glob import glob
+from time import sleep
 
-fileName = "/home/che/Documents/pi_logs/4.csv"
-  
-with open(fileName,'r') as handle:
-    lines = csv.reader(handle, delimiter=',')
-    for row in lines:
-        unixTime.append(int(row[0]))
-        current.append(float(row[1]))
+MAX_LIMIT = 1500
 
+logDir = "/home/che/Documents/pi_logs/*.csv"
+logs = glob(logDir)
 
-plt.plot(unixTime, current)
-plt.xlabel('Time')
-plt.ylabel('Current (A)')
-plt.title('Washing Cycle (Current Draw Over Time)', fontsize = 20)
-plt.show()
+for log in logs:
+    unixTime = []
+    current = []
+    with open(log, 'r') as handle:
+        lines = csv.reader(handle, delimiter=',')
+        limit = 0
+        for row in lines:
+            limit += 1
+            # unixTime.append(int(row[0]))
+            unixTime.append(limit)
+            current.append(float(row[1]))
+            if limit > MAX_LIMIT:
+                break
+
+    plt.plot(unixTime, current)
+    plt.xlabel('Time')
+    plt.ylabel('Current (A)')
+    plt.title('Washing Cycle (Current Draw Over Time)', fontsize=20)
+    plt.savefig(f'{log}.png')
+    plt.close()
