5cebe907f469727e49488f9b5a27647fe02b86b4
josh <joshuasiems@gmail.com>
2022-04-12 17:09:50 -0500

    Add power level server test

--- ui/main.py
+++ ui/main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 socketio = SocketIO()
 socketio.init_app(app, cors_allowed_origins="https://test.theofficialjosh.com")
 
+lr_pl = 5
+
 print("running")
 
 @app.route("/")
@@ -18,23 +20,26 @@ def laundromats():
     lm1 = {
         "name": "cleaners",
         "id": 1200,
-        "num_washers": 2,
-        "num_driers": 2,
-    }
-    lm2 = {
-        "name": "even_cleaners",
-        "id": 42,
         "num_washers": 1,
-        "num_driers": 1,
+        "num_driers": 0,
     }
 
-    response = {"laundromats": [lm1, lm2]}
+    response = {"laundromats": [lm1]}
     return response
 
 @socketio.on("devicePowerUsageRequest")
 def handle_powerUsageRequest(data):
-    data["power_level"] = randrange(10)
+    global lr_pl
+    #data["power_level"] = randrange(10)
+    print("lr_pl in usage request: " + str(lr_pl))
+    data["power_level"] = lr_pl
     emit("devicePowerUsage", data)
+
+@socketio.on("data")
+def handle_data_in(data):
+    global lr_pl
+    lr_pl = data["current"]
+    print("lr_pl in data accept: " + str(lr_pl))
 
 if __name__ == "__main__":
     socketio.run(app, certfile='/etc/letsencrypt/live/test.theofficialjosh.com/fullchain.pem', keyfile='/etc/letsencrypt/live/test.theofficialjosh.com/privkey.pem')
---
+++ ui/test.py
@@ -0,0 +1,19 @@
+import time
+import socketio
+from random import randrange
+
+if __name__ == "__main__":
+    print("running")
+
+    socket = socketio.Client(logger=True)
+
+    socket.connect("http://ec2-18-191-244-170.us-east-2.compute.amazonaws.com")
+    #socket.connect("https://test.theofficialjosh.com")
+
+    while True:
+        print("Sending a message")
+        current = randrange(10)
+        socket.emit('data', {'time': str(int(time.time())), 'current': current, 'ID': 1200})
+        time.sleep(1)
+
+
